28fc44d77b60961e731b0e7348811fbc3b52b641
egidioximenes-glitch <240103015@mhs.udb.ac.id>
2026-06-12 15:21:21 +0700

    peminjaman.php

---
+++ peminjaman.php
@@ -0,0 +1,16 @@
+<?php
+
+header("Content-Type: application/json");
+include 'koneksi.php';
+
+$query = mysqli_query($conn, "SELECT * FROM peminjaman");
+
+$data = [];
+
+while($row = mysqli_fetch_assoc($query)){
+    $data[] = $row;
+}
+
+echo json_encode($data, JSON_PRETTY_PRINT);
+
+?>
